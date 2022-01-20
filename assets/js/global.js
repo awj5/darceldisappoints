03f0c354cfeff19e6e918a11208f27e4f994f8df
Adam Johnson <adamjohnson@apple.com>
2022-01-20 21:09:51 +1100

    Init

---
+++ assets/js/global.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/* Global vars */
+
+window.touchScreen = false;
+
+/* On DOM load */
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Detect mobile
+    window.addEventListener('touchstart', () => {
+        document.querySelector('body').classList.remove('no-touch');
+        window.touchScreen = ! window.touchScreen; // Toggle
+    });
+
+    setEventsGlobal(); // Mouse and keyboard
+
+    // Scroll
+    window.addEventListener('scroll', () => {
+        //window.pageYOffset;
+    });
+
+    // Resize
+    window.addEventListener('resize', () => {
+
+    });
+
+    document.querySelector('html').style.visibility = 'visible'; // Hack to avoid FOUC
+    start();
+});
+
+/* Mouse and keyboard events */
+
+function setEventsGlobal() {}
+
+/* Start */
+
+function start() {}
+
+/* Global */
